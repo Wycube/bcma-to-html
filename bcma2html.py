91c4c5e82a693cffe7065bc45f511110b14e6d88
Wycube <wycube7@gmail.com>
2026-06-06 15:09:11 -0600

    Use html and css writer classes for exporting

--- bcma2html.py
+++ bcma2html.py
@@ -13,7 +13,7 @@ class HTMLWriter:
 
     def start_div(self, css_class=None):
         if css_class is not None:
-            self.add_raw(f"<div class=\"{css_class}\"")
+            self.add_raw(f"<div class=\"{css_class}\">")
         else:
             self.add_raw("<div>")
         self.tabs += 1
@@ -28,11 +28,11 @@ class HTMLWriter:
         self.body += "\n"
 
     def write(self):
-        with open(self.path, "w", encoding="utf-8") as file:
+        with open(self.html_path, "w", encoding="utf-8") as file:
             file.write("<!DOCTYPE html>\n")
             file.write("<html>\n")
             file.write("\t<head>\n")
-            file.write("\t\t<title>bcma2html test</title>\n")
+            file.write(f"\t\t<title>{self.title}</title>\n")
             file.write("\t\t<meta charset=\"utf-8\">\n")
             
             if self.css_path is not None:
@@ -49,10 +49,10 @@ class CSSWriter:
         self.selectors = {}
     
     def add_property(self, class_name, name, value):
-        if self.selectors[class_name] is None:
+        if class_name not in self.selectors:
             self.selectors[class_name] = []
         
-        self.selectors[class_name] += f"{name}: {value};"
+        self.selectors[class_name].append(f"{name}: {value};")
     
     def write(self):
         with open(self.path, "w", encoding="utf-8") as file:
@@ -64,78 +64,63 @@ class CSSWriter:
 
 def export(trees, tex_archives, path, region_lang):
     css_name = os.path.basename(path)
-    with open(f"{path}.html", "w", encoding="utf-8") as html_file, open(f"{path}.css", "w", encoding="utf-8") as css_file:
-        css_file.write("body, div, p {\n")
-        css_file.write("    margin: 0;\n")
-        css_file.write("    padding: 0;\n")
-        css_file.write("    box-sizing: border-box;\n")
-        css_file.write("}\n")
-
-        css_file.write("body {\n")
-        css_file.write("    display: flex;\n")
-        css_file.write("    justify-content: center;\n")
-        css_file.write("}\n")
-
-        css_file.write(".manual {\n")
-        css_file.write("    position: relative;\n")
-        css_file.write("    width: {}px;\n".format(trees[0].canvas_size[0]))
-        css_file.write("    height: {}px;\n".format(trees[0].canvas_size[1]))
-        css_file.write("    white-space: preserve nowrap;\n")
-        css_file.write("}\n")
-
-        html_file.write("<!DOCTYPE html>\n")
-        html_file.write("<html>\n")
-        html_file.write("    <head>\n")
-        html_file.write("        <title>bcma2html test</title>\n")
-        html_file.write("        <meta charset=\"utf-8\">\n")
-        html_file.write(f"        <link rel=\"stylesheet\" href=\"{css_name}.css\">\n")
-        html_file.write("    </head>\n")
-        html_file.write("    <body>\n")
-
-        html_file.write("       <a href=\"../../Home_{}.html\" style=\"position: absolute; left: {}px;\">Home</a>\n".format(region_lang, trees[0].canvas_size[0]))
-
-        html_file.write("       <div class=\"manual\">\n")
-        for tree in trees:
-            convert(tree, tex_archives, tree, path, html_file, css_file)
-        html_file.write("       </div>\n")
-
-        html_file.write("    </body>\n")
-        html_file.write("</html>\n")
+    html = HTMLWriter("bcma2html test", path + ".html", css_name + ".css")
+    css = CSSWriter(path + ".css")
     
+    # body, div, p
+    css.add_property("body, div, p", "margin", "0")
+    css.add_property("body, div, p", "padding", "0")
+    css.add_property("body, div, p", "box-sizing", "border-box")
+
+    # body
+    css.add_property("body", "display", "flex")
+    css.add_property("body", "justify-content", "center")
+
+    # .manual
+    css.add_property(".manual", "position", "relative")
+    css.add_property(".manual", "width", f"{trees[0].canvas_size[0]}px")
+    css.add_property(".manual", "height", f"{trees[0].canvas_size[1]}px")
+    css.add_property(".manual", "white-space", "preserve nowrap")
+
+    html.add_raw(f"<a href=\"../../Home_{region_lang}.html\">Home</a>")
+    html.start_div("manual")
+    for tree in trees:
+        convert(tree, tex_archives, tree, path, html, css)
+    html.end_div()
+
+    html.write()
+    css.write()
+
     split = 0
     for tree in trees:
         with open(f"{path}_{split:03}.txt", "w", encoding="utf-8") as text_file:
             convert_txt(tree, text_file)
         split += 1
 
-def convert(tree, tex_archives, node, path: str, html_file, css_file):
+def convert(tree, tex_archives, node, path: str, html, css):
     if type(node) == layout.Pane:
-        html_file.write("<div class=\"{}\">\n".format(node.name))
+        html.start_div(node.name)
 
-        css_file.write(".{} {{\n".format(node.name))
-        css_file.write("    position: absolute;\n")
-        css_file.write("    left: {}px;\n".format(node.pane_data.translation[0]))
-        css_file.write("    top: {}px;\n".format(-node.pane_data.translation[1]))
-        css_file.write("    width: {}px;\n".format(node.pane_data.size[0]))
-        css_file.write("    height: {}px;\n".format(node.pane_data.size[1]))
-        css_file.write("}\n")
+        css.add_property(f".{node.name}", "position", "absolute")
+        css.add_property(f".{node.name}", "left", f"{node.pane_data.translation[0]}px")
+        css.add_property(f".{node.name}", "top", f"{-node.pane_data.translation[1]}px")
+        css.add_property(f".{node.name}", "width", f"{node.pane_data.size[0]}px")
+        css.add_property(f".{node.name}", "height", f"{node.pane_data.size[1]}px")
 
-    # if type(node) == Canvas or type(node) == Pane:
     if type(node) == layout.LayoutTree or type(node) == layout.Pane:
         for child in node.children:
-            convert(tree, tex_archives, child, path, html_file, css_file)
+            convert(tree, tex_archives, child, path, html, css)
     elif type(node) == layout.Text:
-        html_file.write("<p class=\"{}\">{}</p>\n".format(node.name, node.text))
+        html.add_raw(f"<p class=\"{node.name}\">{node.text}</p>")
 
-        css_file.write(".{} {{\n".format(node.name))
-        css_file.write("    position: absolute;\n")
-        css_file.write("    left: {}px;\n".format(node.pane_data.translation[0]))
-        css_file.write("    top: {}px;\n".format(-node.pane_data.translation[1]))
-        css_file.write("    width: {}px;\n".format(node.pane_data.size[0]))
-        css_file.write("    height: {}px;\n".format(node.pane_data.size[1]))
-        css_file.write("    font-size: {}px;\n".format(node.font_scale[1]))
-        css_file.write("    color: #{:08X};\n".format(struct.unpack("<I", struct.pack(">I", node.top_color))[0]))
-        css_file.write("}\n")
+        font_color = struct.unpack("<I", struct.pack(">I", node.top_color))[0]
+        css.add_property(f".{node.name}", "position", "absolute")
+        css.add_property(f".{node.name}", "left", f"{node.pane_data.translation[0]}px")
+        css.add_property(f".{node.name}", "top", f"{-node.pane_data.translation[1]}px")
+        css.add_property(f".{node.name}", "width", f"{node.pane_data.size[0]}px")
+        css.add_property(f".{node.name}", "height", f"{node.pane_data.size[1]}px")
+        css.add_property(f".{node.name}", "font-size", f"{node.font_scale[1]}px")
+        css.add_property(f".{node.name}", "color", f"#{font_color:08X}")
     elif type(node) == layout.Picture:
         assert node.tex_coords == [(0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0)], f"Texcoords other than default not handled yet! {node.tex_coords}"
         for col in node.tex_data[:16]:
@@ -148,21 +133,20 @@ def convert(tree, tex_archives, node, path: str, html_file, css_file):
         mat_index = node.tex_data[16]
         tex_index = mat_list.materials[mat_index][1][0][0]
         tex_name: str = tex_list.textures[tex_index]
+        tex_name_png = tex_name.replace(".bclim", ".png")
 
-        html_file.write("<img src=\"{}\" class=\"{}\">".format(tex_name.replace(".bclim", ".png"), node.name))
+        html.add_raw(f"<img src=\"{tex_name_png}\" class=\"{node.name}\">")
 
-        css_file.write(".{} {{\n".format(node.name))
-        css_file.write("    position: absolute;\n")
-        css_file.write("    left: {}px;\n".format(node.pane_data.translation[0]))
-        css_file.write("    top: {}px;\n".format(-node.pane_data.translation[1]))
-        css_file.write("    width: {}px;\n".format(node.pane_data.size[0]))
-        css_file.write("    height: {}px;\n".format(node.pane_data.size[1]))
-        css_file.write("}\n")
+        css.add_property(f".{node.name}", "position", "absolute")
+        css.add_property(f".{node.name}", "left", f"{node.pane_data.translation[0]}px")
+        css.add_property(f".{node.name}", "top", f"{-node.pane_data.translation[1]}px")
+        css.add_property(f".{node.name}", "width", f"{node.pane_data.size[0]}px")
+        css.add_property(f".{node.name}", "height", f"{node.pane_data.size[1]}px")
 
         for darc in tex_archives:
             if darc.has_file("./timg/" + tex_name):
                 bclim = texture.BCLIM(darc.get_file("./timg/" + tex_name))
-                bclim.save_as_png(path[:path.rfind('/')] + "/" + tex_name.replace(".bclim", ".png"))
+                bclim.save_as_png(path[:path.rfind('/')] + "/" + tex_name_png)
     elif type(node) == layout.Window:
         mat_list = tree.get_toplevel_obj_of_type(layout.MaterialList)
         tex_list = tree.get_toplevel_obj_of_type(layout.TextureList)
@@ -170,38 +154,39 @@ def convert(tree, tex_archives, node, path: str, html_file, css_file):
         
         mat_index = node.cont_data[4]
         tex_index = mat_list.materials[mat_index][1]
+        back_color = struct.unpack("<I", struct.pack(">I", node.cont_data[0]))[0]
 
         node.load_texture_sizes(mat_list, tex_list, tex_archives)
-        css_file.write(".{} {{\n".format(node.name))
-        css_file.write("    position: absolute;\n")
-        css_file.write("    left: {}px;\n".format(node.pane_data.translation[0]))
-        css_file.write("    top: {}px;\n".format(-node.pane_data.translation[1]))
-        css_file.write("    width: {}px;\n".format(node.content_box[1] - node.content_box[0]))
-        css_file.write("    height: {}px;\n".format(node.content_box[3] - node.content_box[2]))
-        css_file.write("    background-color: #{:08X};\n".format(struct.unpack("<I", struct.pack(">I", node.cont_data[0]))[0]))
+        css.add_property(f".{node.name}", "position", "absolute")
+        css.add_property(f".{node.name}", "left", f"{node.pane_data.translation[0]}px")
+        css.add_property(f".{node.name}", "top", f"{-node.pane_data.translation[1]}px")
+        css.add_property(f".{node.name}", "width", f"{node.content_box[1] - node.content_box[0]}px")
+        css.add_property(f".{node.name}", "height", f"{node.content_box[3] - node.content_box[2]}px")
+        css.add_property(f".{node.name}", "background-color", f"#{back_color:08X}")
 
         if node.border_image is not None:
-            css_file.write("    border: solid transparent;")
-            css_file.write("    border-left-width: {}px;\n".format(node.content_box[0]))
-            css_file.write("    border-right-width: {}px;\n".format(node.pane_data.size[0] - node.content_box[1]))
-            css_file.write("    border-top-width: {}px;\n".format(node.content_box[2]))
-            css_file.write("    border-bottom-width: {}px;\n".format(node.pane_data.size[1] - node.content_box[3]))
-            css_file.write("    border-image: url(\"{}\");\n".format(path[path.rfind('/') + 1:] + "_" + node.name + ".png"))
-            css_file.write("    border-image-slice: {} {} {} {};\n".format(node.content_box[2], node.content_box[0], node.pane_data.size[1] - node.content_box[3], node.pane_data.size[0] - node.content_box[1]))
-            css_file.write("    box-sizing: content-box;")
-            css_file.write("    background-clip: padding-box;")
+            border_sizes = (node.content_box[2], node.pane_data.size[0] - node.content_box[1], node.pane_data.size[1] - node.content_box[3], node.content_box[0])
+            css.add_property(f".{node.name}", "border", "solid transparent")
+            css.add_property(f".{node.name}", "border-top-width", f"{border_sizes[0]}px")
+            css.add_property(f".{node.name}", "border-right-width", f"{border_sizes[1]}px")
+            css.add_property(f".{node.name}", "border-bottom-width", f"{border_sizes[2]}px")
+            css.add_property(f".{node.name}", "border-left-width", f"{border_sizes[3]}px")
+            css.add_property(f".{node.name}", "border-image", "url(\"{}\")".format(path[path.rfind('/') + 1:] + "_" + node.name + ".png"))
+            css.add_property(f".{node.name}", "border-image-slice", "{} {} {} {}".format(*border_sizes))
+            css.add_property(f".{node.name}", "box-sizing", "content-box")
+            css.add_property(f".{node.name}", "background-clip", "padding-box")
         
         if len(tex_index) == 1:
             assert tex_list is not None, "Texture list missing!"
             tex_name: str = tex_list.textures[tex_index[0][0]]
-            css_file.write("background-image: url(\"{}\");".format(tex_name.replace(".bclim", ".png")))
+            tex_name_png = tex_name.replace(".bclim", ".png")
+            css.add_property(f".{node.name}", "background-image", "url(\"{}\")".format(tex_name_png))
             for darc in tex_archives:
                 if darc.has_file("./timg/" + tex_name):
                     bclim = texture.BCLIM(darc.get_file("./timg/" + tex_name))
-                    bclim.save_as_png(path[:path.rfind('/')] + "/" + tex_name.replace(".bclim", ".png"))
+                    bclim.save_as_png(path[:path.rfind('/')] + "/" + tex_name_png)
 
-        css_file.write("}\n")
-        html_file.write("<div class=\"{}\"></div>".format(node.name))
+        html.add_raw(f"<div class=\"{node.name}\"></div>")
 
         if node.border_image is not None:
             export = PIL.Image.new("RGBA", node.border_image_size)
@@ -209,7 +194,7 @@ def convert(tree, tex_archives, node, path: str, html_file, css_file):
             export.save(path + "_" + node.name + ".png", format="png")
 
     if type(node) == layout.Pane:
-        html_file.write("</div>\n")
+        html.end_div()
 
 def convert_txt(tree, text_file):
     text = tree.print()
